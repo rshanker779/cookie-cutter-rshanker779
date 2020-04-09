5b98a4980a63c6619e6fca5c2eba0411c5fea6a4
rohan <rshanker779@gmail.com>
2020-04-09 22:22:29 +0100

    Using pathlib to read file

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -1,13 +1,14 @@
+import pathlib
+
 from setuptools import setup, find_packages
-import os
+
 try:
-    with open(os.path.join(os.path.dirname(__file__)), 'README.md') as f:
-        long_description = f.read()
+    long_description = (pathlib.Path(__file__).parent / 'README.md').read_text()
 except:
     long_description = None
 setup(
     name="{{cookiecutter.project_name}}",
-    version="1.0.0",
+    version="0.0.1",
     author="{{cookiecutter.author}}",
     author_email="{{cookiecutter.email}}",
     description="{{cookiecutter.description}}",
@@ -16,9 +17,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         "rshanker779_common",
-        "coverage",
     ],
     packages=find_packages(),
     entry_points={},
-    test_suite="{{cookiecutter.project_name}}/tests",
 )
